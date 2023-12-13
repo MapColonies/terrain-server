77bb55050853e283e0ca2b5b33367cb7c41866cb
ellamartirosyan <ellamartirosyan@gmail.com>
2023-12-13 21:54:28 +0200

    fix: final version with better-sqlite3 and better-sqlite-pool

--- index.js
+++ index.js
@@ -1,22 +1,15 @@
 const express = require('express');
-const sqlite3 = require('better-sqlite3');
-const genericPool = require('generic-pool');
+const { Pool } = require('better-sqlite-pool');
 const cors = require('cors');
 const path = require('path');
 const fs = require('fs');
 
 const PORT = process.env.PORT || 8000;
-const MAX_CONNECTIONS = process.env.MAX_CONNECTIONS || 100;
-const MIN_CONNECTIONS = process.env.MIN_CONNECTIONS || 10;
 const TERRAIN_TILES_TABLE = 'terrain_tiles';
 const LAYER_JSON_TABLE = 'layer_json';
 const GPKG_PATH = '/data';
 const SUFFIX = '.gpkg';
 const terrains = new Map();
-const OPTS = {
-  max: MAX_CONNECTIONS, // maximum number of connections to create at any given time (default: 1)
-  min: MIN_CONNECTIONS // minimum number of connections to keep in pool at any given time (default: 0)
-};
 
 const app = express();
 
@@ -24,63 +17,29 @@ app.use(cors());
 
 fs.readdirSync(GPKG_PATH).forEach(file => {
   if (file.endsWith(SUFFIX)) {
-    const factory = {
-      create: function() {
-        return new sqlite3(path.join(GPKG_PATH, file), { readonly: true });
-      },
-      destroy: function(db) {
-        db.close();
-      }
-    };
-    const pool = genericPool.createPool(factory, OPTS);
+    const pool = new Pool(path.join(GPKG_PATH, file));
     terrains.set(path.parse(file).name, pool);
   }
 });
 
 app.get('/terrains/:gpkg/:z/:x/:y.terrain', async (request, response) => {
   const { gpkg, z, x, y } = request.params;
-  let gpkgPool;
-  let connection;
-  try {
-    const query = `SELECT tile_data FROM ${TERRAIN_TILES_TABLE} WHERE zoom_level = ? AND tile_column = ? AND tile_row = ?`;
-
-    gpkgPool = terrains.get(gpkg);
-    connection = await gpkgPool.acquire();
-    const stmt = connection.prepare(query);
-    const result = stmt.get(z, x, y);
-    await gpkgPool.release(connection);
-
-    response.header('Content-Encoding', 'gzip');
-    response.type('application/octet-stream');
-    response.send(result.tile_data);
-  } catch (err) {
-    if (connection) {
-      await gpkgPool.release(connection);
-    }
-    response.status(500).json({ error: err.message });
-  }
+  const pool = terrains.get(gpkg);
+  const db = await pool.acquire();
+  const result = db.prepare(`SELECT tile_data FROM ${TERRAIN_TILES_TABLE} WHERE zoom_level = ? AND tile_column = ? AND tile_row = ?`).get(z, x, y);
+  response.header('Content-Encoding', 'gzip');
+  response.type('application/octet-stream');
+  response.send(result.tile_data);
+  db.release();
 });
 
 app.get('/terrains/:gpkg/layer.json', async (request, response) => {
   const { gpkg } = request.params;
-  let gpkgPool;
-  let connection;
-  try {
-    const query = `SELECT data FROM ${LAYER_JSON_TABLE}`;
-
-    gpkgPool = terrains.get(gpkg);
-    connection = await gpkgPool.acquire();
-    const stmt = connection.prepare(query);
-    const result = stmt.get();
-    await gpkgPool.release(connection);
-
-    response.json(JSON.parse(result.data));
-  } catch (err) {
-    if (connection) {
-      await gpkgPool.release(connection);
-    }
-    response.status(500).json({ error: err.message });
-  }
+  const pool = terrains.get(gpkg);
+  const db = await pool.acquire();
+  const result = db.prepare(`SELECT data FROM ${LAYER_JSON_TABLE}`).get();
+  response.json(JSON.parse(result.data));
+  db.release();
 });
 
 app.get('/health', async (request, response) => {
