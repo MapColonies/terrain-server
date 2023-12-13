29a6b13bedc6e68402356d020f8fa57f611c99cf
ellamartirosyan <ellamartirosyan@gmail.com>
2023-12-13 21:45:54 +0200

    fix: final version with better-sqlite3 and generic-pool

--- index.js
+++ index.js
@@ -24,7 +24,6 @@ app.use(cors());
 
 fs.readdirSync(GPKG_PATH).forEach(file => {
   if (file.endsWith(SUFFIX)) {
-    // Create a factory object for creating SQLite connections
     const factory = {
       create: function() {
         return new sqlite3(path.join(GPKG_PATH, file), { readonly: true });
@@ -33,10 +32,7 @@ fs.readdirSync(GPKG_PATH).forEach(file => {
         db.close();
       }
     };
-
     const pool = genericPool.createPool(factory, OPTS);
-
-    // Add a pool for the current gpkg file to the map
     terrains.set(path.parse(file).name, pool);
   }
 });
@@ -49,19 +45,15 @@ app.get('/terrains/:gpkg/:z/:x/:y.terrain', async (request, response) => {
     const query = `SELECT tile_data FROM ${TERRAIN_TILES_TABLE} WHERE zoom_level = ? AND tile_column = ? AND tile_row = ?`;
 
     gpkgPool = terrains.get(gpkg);
-    // Acquire a connection from the gpkg's pool
     connection = await gpkgPool.acquire();
-    console.log(' BEFORE ACQ gpkgPool.available=', gpkgPool.available);
     const stmt = connection.prepare(query);
     const result = stmt.get(z, x, y);
     await gpkgPool.release(connection);
-    console.log(' AFTER ACQ gpkgPool.available=', gpkgPool.available);
 
     response.header('Content-Encoding', 'gzip');
     response.type('application/octet-stream');
     response.send(result.tile_data);
   } catch (err) {
-    // If an error occurs, ensure the connection is released back to the pool
     if (connection) {
       await gpkgPool.release(connection);
     }
@@ -77,7 +69,6 @@ app.get('/terrains/:gpkg/layer.json', async (request, response) => {
     const query = `SELECT data FROM ${LAYER_JSON_TABLE}`;
 
     gpkgPool = terrains.get(gpkg);
-    // Acquire a connection from the gpkg's pool
     connection = await gpkgPool.acquire();
     const stmt = connection.prepare(query);
     const result = stmt.get();
@@ -85,7 +76,6 @@ app.get('/terrains/:gpkg/layer.json', async (request, response) => {
 
     response.json(JSON.parse(result.data));
   } catch (err) {
-    // If an error occurs, ensure the connection is released back to the pool
     if (connection) {
       await gpkgPool.release(connection);
     }
@@ -98,5 +88,5 @@ app.get('/health', async (request, response) => {
 });
 
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log(`Server is listening on port ${PORT}`);
 });
